14933b9e4cd0c8cf7f274b3f15f6bcd5c71b10a8
vlad <b.vlad87@mail.ru>
2022-08-03 14:39:14 +0300

    Lession_1

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        ordering = ['pk']
+
+    def __str__(self):
+        return f'{self.username} | {self.email} | {self.first_name} {self.last_name}'
